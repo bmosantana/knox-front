aa7deb833ec0280118d6b834e15c1dbbb7e94cde
bmosantana <fantasmsant@gmail.com>
2019-05-08 01:45:50 -0300

    Steppers adicionados na página de cliente

--- src/components/CadastroCliente.js
+++ src/components/CadastroCliente.js
@@ -2,12 +2,61 @@ import React, { Component } from 'react';
 import MenuLateral from './MenuLateral.js';
 import TextField from '@material-ui/core/TextField';
 import '../css/CadastroCliente.css';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
+const styles = theme => ({
+  root: {
+    width: '90%',
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+  resetContainer: {
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return `For each ad campaign that you create, you can control how much
+              you're willing to spend on clicks and conversions, which networks
+              and geographical locations you want your ads to show on, and more.`;
+    case 1:
+      return 'An ad group contains one or more ads which target a shared set of keywords.';
+    case 2:
+      return `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`;
+    default:
+      return 'Unknown step';
+  }
+}
 
 class CadastroCliente extends Component{
     
     constructor(props) {
         super(props)
+        this.state = {
+          activeStep: 0,
+        };
         
     }
 
@@ -15,11 +64,36 @@ class CadastroCliente extends Component{
     //     this.setState({modalState: newProps.modalState})
     // };
 
+    
+  
+    handleNext = () => {
+      this.setState(state => ({
+        activeStep: state.activeStep + 1,
+      }));
+    };
+  
+    handleBack = () => {
+      this.setState(state => ({
+        activeStep: state.activeStep - 1,
+      }));
+    };
+  
+    handleReset = () => {
+      this.setState({
+        activeStep: 0,
+      });
+    };
+  
+    
+  
     handleSubmit = () =>{
       alert("cad")
-    }
+    };
 
-    render(){
+    render() {
+      const { classes } = this.props;
+      const steps = getSteps();
+      const { activeStep } = this.state;
         return (
           <div id="page-all">
             <MenuLateral/>
@@ -30,72 +104,43 @@ class CadastroCliente extends Component{
               <div>
                 <p id="titulo-form">Cadastro de Cliente</p>
               </div>
-              
-              {/* Inicio do Formulário */}
-              <form method="get" onSubmit={this.handleSubmit}>
-                {/* Div que contem os primeiros campos */}
+              <Stepper activeStep={activeStep} orientation="vertical">
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
 
-                {/* Campo de Nome */}
-                <TextField
-                  className="textfield-cli"
-                  id="nome"
-                  label="Nome"
-                  margin="normal"
-                  type="text"
-                  // value={this.props.nome}
-                  // onChange={this.handleChange('email')}
-                  style = {{width: "90vw"}}
-                />
-
-                <TextField
-                  id="cpf"
-                  label="CPF"
-                  margin="normal"
-                  type="text"
-                  style = {{width: "30vw", paddingRight: '0px'}}
-                />
-
-                <TextField
-                  id="rg"
-                  label="RG"
-                  margin="normal"
-                  type="text"
-                  style = {{width: "30vw"}}
-                />
-
-                <TextField
-                  id="pis"
-                  label="Numero do PIS"
-                  margin="normal"
-                  type="text"
-                  style = {{width: "90vw"}}
-                />
-
-                <TextField
-                  id="carteira-trabalho"
-                  label="Numero da Carteira de Trabalho"
-                  margin="normal"
-                  type="text"
-                  style = {{width: "90vw"}}
-                />
-
-                <TextField
-                  id="logradouro"
-                  label="Logradouro"
-                  margin="normal"
-                  type="text"
-                  style = {{width: "90vw"}}
-                />
-
-                <TextField
-                  id="logradouro"
-                  label="Logradouro"
-                  margin="normal"
-                  type="text"
-                  style = {{width: "90vw"}}
-                />
-
-              </form>
+              {/* aqui vai os campos do form */}
+              <StepContent>
+                <Typography>{getStepContent(index)}</Typography>
+                <div >
+                  <div>
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={this.handleBack}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                    >
+                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                  </div>
+                </div>
+              </StepContent>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length && (
+          <Paper square elevation={0}>
+            <Typography>All steps completed - you&apos;re finished</Typography>
+            <Button onClick={this.handleReset} >
+              Reset
+            </Button>
+          </Paper>
+        )}
             </div>
 
           </div>
